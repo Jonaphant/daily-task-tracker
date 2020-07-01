f34dc37b060de7d70d376f42a419fb25e701c87b
Jonathan Sou <jonathansou7@gmail.com>
2020-06-30 18:01:17 -0700

    Styled basic navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Login from './components/auth/Login';
+
+import './App.css';
 
 // Material UI
 import CssBaseline from '@material-ui/core/CssBaseline';
 
-const App = () => (
-  <React.Fragment>
-    <CssBaseline />
-    <Navbar />
-    <Login />
-  </React.Fragment>
-);
+const App = () => {
+  return (
+    <Router>
+      <React.Fragment>
+        <CssBaseline />
+        <Navbar />
+        <Login />
+      </React.Fragment>
+    </Router>
+  );
+};
 
 export default App;
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,10 +1,45 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Grid from '@material-ui/core/Grid';
+import ButtonBase from '@material-ui/core/ButtonBase';
+import Typography from '@material-ui/core/Typography';
 
 const Navbar = () => {
   return (
     <React.Fragment>
-      <Grid item xs={12} style={{ backgroundColor: '#cfe8fc' }}></Grid>
+      <Grid
+        container
+        justify="center"
+        style={{ backgroundColor: 'lightblue', padding: '5px' }}
+      >
+        <Grid item container xs={12} spacing={1}>
+          <Grid item xs={6}>
+            <ButtonBase>
+              <Link to="/">
+                <Typography variant="h3">Daily Task Tracker</Typography>{' '}
+              </Link>
+            </ButtonBase>
+          </Grid>
+          <Grid item container xs={6} justify="flex-end" alignItems="center">
+            <Grid item container xs={3} justify="space-evenly">
+              <Grid item xs={4}>
+                <ButtonBase>
+                  <Link to="/login">
+                    <Typography variant="h6">Login</Typography>
+                  </Link>
+                </ButtonBase>
+              </Grid>
+              <Grid item xs={4}>
+                <ButtonBase>
+                  <Link to="/register">
+                    <Typography variant="h6">Register</Typography>
+                  </Link>
+                </ButtonBase>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
     </React.Fragment>
   );
 };
